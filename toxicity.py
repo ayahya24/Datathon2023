ed8535c003d07675be2e88c86e55285e2575242e
ayahya24 <55040802+ayahya24@users.noreply.github.com>
2022-10-09 10:37:04 -0500

    Fixed other merge conflict

--- toxicity.py
+++ toxicity.py
@@ -15,11 +15,7 @@ examples=[Example("kys", "Toxic"),
 
 # TODO: Implement this
 def isToxic(text):
-<<<<<<< HEAD
     response = co.classify(model='f6661bd3-106c-470a-ac3f-3a6b1eec4a7b-ft', inputs=[text], examples=examples)
-=======
-    response = co.classify(model='f6661bd3-106c-470a-ac3f-3a6b1eec4a7b-ft', inputs=[text])
->>>>>>> 504f7202b969d19de52a7134b7e5e8e27d36c053
     print(response.classifications[0].labels['Toxic'].confidence)
     if response.classifications[0].labels['Toxic'].confidence > 0.70:
         return True
