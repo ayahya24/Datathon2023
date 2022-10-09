af5ac7df94e3ece23dee74f24be4614b2515f357
ayahya24 <55040802+ayahya24@users.noreply.github.com>
2022-10-09 10:34:57 -0500

    Fixed model name

--- toxicity.py
+++ toxicity.py
@@ -1,4 +1,5 @@
 import cohere
+from cohere.classify import Example
 
 COHERE_TOKEN = ''
 with open("coheretoken.txt", 'r') as file:
@@ -6,21 +7,16 @@ with open("coheretoken.txt", 'r') as file:
 
 co = cohere.Client(COHERE_TOKEN)
 print (COHERE_TOKEN)
-'''
-test = [
-    "Hi its nice to meet you!",
-    "You should kill yourself NOW!"
-]
-response = co.classify(
-    model = 'cohere-toxicity',
-    inputs = test)
 
-print(response.classifications)
-'''
+examples=[Example("kys", "Toxic"),
+         Example("gg ez", "Toxic"),
+         Example("family guy", "Not Toxic"),
+         Example("Family Man", "Not Toxic")]
+
 # TODO: Implement this
 def isToxic(text):
-    response = co.classify(model='f6661bd3-106c-470a-ac3f-3a6b1eec4a7b-ft', inputs=[text])
-    print(response.classifications[0].labels['TOXIC'].confidence)
-    if response.classifications[0].labels['TOXIC'].confidence > 0.70:
+    response = co.classify(model='f6661bd3-106c-470a-ac3f-3a6b1eec4a7b-ft', inputs=[text], examples=examples)
+    print(response.classifications[0].labels['Toxic'].confidence)
+    if response.classifications[0].labels['Toxic'].confidence > 0.70:
         return True
     return False
